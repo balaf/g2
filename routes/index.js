cfc2d1d107d6bf754143b4357edb05149a83d86e
Elias Balafoutis <balafi@myopera.com>
2013-11-23 01:08:49 +0200

    hack blueimp-gallery

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,20 @@
 exports.index = function(req, res){
   var data = req.gallery;
 
-  res.render('home', data.album);
+
+  res.render('home', { title: "Ζωγραφική Τοίχου - Παιδικό Δωμάτιο - Δέσποινα Τσίγκα Μπούσια", "gallery" : data.album});
 };
 
 exports.work = function (req, res) {
     var data = req.gallery;
-
     console.log(JSON.stringify(data))
-    res.render('all', data.album);
+    res.render('all', { title:"Έργα- Δέσποινα Τσίγκα Μπούσια" , gallery: data.album });
 }
 
 
 exports.gallery = function(req, res){
     var data = req.gallery;
-    res.render('work', {album: data.album,
+    res.render('work', {title: "hello",
+                        album: data.album,
                         path: data.album.path});
 };
